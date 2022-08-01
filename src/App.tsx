638081aef22010248f0251b3a6bf5ed716d85cb9
Jahid Hasan <jahidhasan2856@gmail.com>
2022-08-01 17:12:18 +0600

    something update with toast add

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 import './App.css'
 import InputForm from './components/InputForm/InputForm';
 
@@ -6,24 +7,27 @@ function App() {
 
 
   return (
-
-    <div className='' style={{ background: '#d3cfcf' }}>
-      <nav className="bg-dark">
-        <h2 className="text-white text-center p-2 text-uppercase">Place Holder For a logo</h2>
-      </nav>
-      <div className='p-5'>
-        <div className='bg-white p-5 rounded-2'>
-          <h4 className='text-uppercase text-center shadow p-3'>Text Here</h4>
-          <div>
-            <InputForm />
-          </div>
-          <div>
-
+    <>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+      />
+      <div className='' style={{ background: '#d3cfcf' }}>
+        <nav className="bg-dark">
+          <h2 className="text-white text-center p-2 text-uppercase">Place Holder For a logo</h2>
+        </nav>
+        <div className='p-5'>
+          <div className='bg-white p-5 rounded-2'>
+            <h4 className='text-uppercase text-center shadow p-3'>Text Here</h4>
+            <div>
+              <InputForm />
+            </div>
+            <div>
+            </div>
           </div>
         </div>
       </div>
-
-    </div>
+    </>
   );
 }
 
--- src/components/InputForm/InputForm.tsx
+++ src/components/InputForm/InputForm.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
+import toast from 'react-hot-toast';
 const InputForm = () => {
+    const [disable, setDisable] = useState(false);
 
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-
-    const onSubmit = (data: any) => console.log(data);
-
     const { site_config } = watch();
-    console.log()
+
+
+    const onSubmit = (data: any) => {
+        setDisable(true)
+
+        setTimeout(() => {
+            setDisable(false)
+            toast.success('Submit successfully')
+        }, 3000);
+    };
+
     return (
         <>
             <form onSubmit={handleSubmit(onSubmit)} className=''>
@@ -34,8 +43,8 @@ const InputForm = () => {
                         <span className='text-danger'>
                             {errors.site_config && "This field is required"}</span>
                         <p className='text_characters text-end mb-0'>
-                            {site_config?.length}
-                            out of 500 characters</p>
+                            {site_config?.length || 0} out of 500 characters</p>
+
                     </div>
                 </div>
                 <div className='mt-4 d-flex gap-2 align-items-center'>
@@ -46,8 +55,8 @@ const InputForm = () => {
                     <p className={`mb-0 ${errors.confirm ? 'text-danger' : ''}`}>Entered the site config</p>
                 </div>
                 <div className='d-flex justify-content-end gap-3'>
-                    <button className='btn btn-outline-secondary'>Cencel</button>
-                    <button className='btn btn-success'>Confirm</button>
+                    <button type='reset' className='btn btn-outline-secondary'>Cencel</button>
+                    <button disabled={disable} type='submit' className='btn btn-success'>Confirm</button>
                 </div>
             </form>
         </>
